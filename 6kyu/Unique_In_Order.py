71303aaa56aa72b66046767bcf9dfa408a23fd63
marsiekiera <hatchet17@gmail.com>
2021-01-08 21:41:28 +0100

    Create file

---
+++ 6kyu/Unique_In_Order.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/54e6533c92449cc251001667
+
+def unique_in_order(iterable):
+    result = []
+    prev = None
+    for char in iterable[0:]:
+        if char != prev:
+            result.append(char)
+            prev = char
+    return result
